ea6d1ca188dbfc1b79c02f1cb4f5b285a0346383
Vincent Covelli <vincent.covelli78@gmail.com>
2025-03-05 19:29:49 -0500

    Added logging configuration script

---
+++ scripts/logging_config.py
@@ -0,0 +1,16 @@
+import logging
+
+def setup_logging(log_file='app.log'):
+    # Set up logging to both console and a log file
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        handlers=[
+            logging.FileHandler(log_file),
+            logging.StreamHandler()
+        ]
+    )
+
+    logger = logging.getLogger(__name__)
+    return logger
+
